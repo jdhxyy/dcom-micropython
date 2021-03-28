445e4b45213b0ef6aab0097897a565b3cb23329d
jdhxyy <jdh821@163.com>
2021-03-28 20:07:59 +0800

    Defect:REQ022.003 Modify:zhouxin Desc:编写micropython版本的dcom

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dcompy
+# micropython-dcompy
 
 ## 介绍
 dcompy:Device Communication Protocol(DCOM)：设备间通信协议。 DCOM协议可用于物联网设备之间RPC通信。
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 filepath = 'README.md'
 setup(
-        name="dcompy",
-        version="1.0",
-        description="dcom python package",
+        name="micropython-dcompy",
+        version="1.1",
+        description="dcom micropython package",
         long_description=open(filepath, encoding='utf-8').read(),
         long_description_content_type="text/markdown",
         author="jdh99",
         author_email="jdh821@163.com",
-        url="https://github.com/jdhxyy/dcom-python",
+        url="https://github.com/jdhxyy/dcom-micropython",
         packages=find_packages(),
         data_files=[filepath]
     )
